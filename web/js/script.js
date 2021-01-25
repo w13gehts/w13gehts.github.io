b5fa2d07ef65641320ed4d6b7991adb5dbccf101
Juliana <juliana.salafia@gmail.com>
2021-01-25 18:23:11 -0300

    formulario final.

--- web/js/script.js
+++ web/js/script.js
@@ -12,3 +12,11 @@ new SimpleSlide(
 if(window.SimpleAnime) {
 new SimpleAnime ();
 }
+if(window.SimpleForm) {
+new SimpleForm({
+  form: ".formphp",
+  button: "#enviar",
+  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente entrar em contato no email juliana.salafia@gmail.com</p></div>",
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>"
+});
+}
